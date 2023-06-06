ec78f728674e8f50e23df8bd4e3f919637323007
zdeep10 <gabriel.wti02@gmail.com>
2023-06-06 16:30:48 -0300

    Prototipo Grosseiro Atlantic-marketfit 1/2

---
+++ pages/atlantic-marketfit.php
@@ -0,0 +1,95 @@
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/header.php';
+?>
+
+<!-- BANNER INTRO -->
+<!-- <section id="banner-intro">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col">
+                <h1>ATLANTIC <br> MARKETFIT</h1>
+                <p>O estudo detalhado sobre o mercado e relatório de aderência, elaborado por profissionais experientes e especializados.
+                </p>
+                <a class="btn btn-primary" href="">ADQUIRA AGORA</a>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- POTENCIAL -->
+<!-- <section id="potencial">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <figure class="col mb-0">
+                <img loading="lazy" class="img-fluid" src="" alt="Thumb seção">
+            </figure>
+
+            <div class="col-12">
+                <h2>Qual é o potencial da sua empresa para a internacionalização?</h2>
+                <p>O diagnóstico é o primeiro passo da internacionalização. Você precisa de informações específicas relacionadas aos seus produtos ou serviços, em relação ao mercado português.
+                    <span>
+                        <strong>
+                            O MarketFit
+                        </strong>
+                        foi criado para coletar informações, analisar dados e fornecer um relatório de viabilidade para os seus negócios internacionais, de forma precisa e personalizada.
+                    </span>
+                </p>
+            </div>
+
+            <div class="col-12">
+                <h2>Junte-se às empresas que usam e confiam no Atlantic Marketfit</h2>
+
+                *carousel com empresas
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- ESTUDO -->
+<!-- <section id="estudo">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col-12">
+                <figure class="mb-0 d-flex">
+                    <img loading="lazy" class="img-fluid" src="" alt="Thumb">
+                    <h2>Por que devo realizar este estudo?</h2>
+                </figure>
+            </div>
+
+            <div class="col-12">
+                <figure class="d-flex mb-0">
+                    <img loading="lazy" class="img-fluid" src="" alt="O marketfit">
+                    <p>O <strong> MarketFit</strong> está preparado para apresentar as melhores soluções e possibilidades para empresários brasileiros que desejam abrir seus negócios em Portugal.</p>
+                </figure>
+            </div>
+            <div class="col-12">
+                <figure class="d-flex mb-0">
+                    <img loading="lazy" class="img-fluid" src="" alt="Equipe">
+                    <p>Uma equipe de especialistas, com larga experiência no mercado português vai oferecer um elenco de caminhos para a sua jornada de internacionalização e conduzi-lo para o próximo estágio do processo: o <strong>Atlantic Scale-out.</strong></p>
+                </figure>
+            </div>
+
+            <div class="col-12">
+                <a href="" target="_blank">CONTRATE AGORA</a>
+            </div>
+        </div>
+    </div>
+</section> -->
+
+<!-- THUMB SECAO -->
+<section id="thumb-secao">
+    <div class="container px-4">
+        <div class="row">
+            <figure class="col mb-0">
+                <img loading="lazy" class="img-fluid" src="" alt="LAPTOP">
+                <img loading="lazy" class="img-fluid" src="" alt="Dados tecnicos">
+            </figure>
+        </div>
+    </div>
+</section>
+
+<?php
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/partials/footer.php';
+?>
