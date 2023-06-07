6e9d258e23c9b03ff0c53da4a52b44c928769213
zdeep10 <gabriel.wti02@gmail.com>
2023-06-07 08:24:36 -0300

    Prototipagem grosseira atlantic-marketfit 2/2

--- pages/atlantic-marketfit.php
+++ pages/atlantic-marketfit.php
@@ -4,7 +4,7 @@ require $root . '/partials/header.php';
 ?>
 
 <!-- BANNER INTRO -->
-<!-- <section id="banner-intro">
+<section id="banner-intro">
     <div class="container px-4 py-5">
         <div class="row">
             <div class="col">
@@ -15,10 +15,10 @@ require $root . '/partials/header.php';
             </div>
         </div>
     </div>
-</section> -->
+</section>
 
 <!-- POTENCIAL -->
-<!-- <section id="potencial">
+<section id="potencial">
     <div class="container px-4 py-5">
         <div class="row">
             <figure class="col mb-0">
@@ -44,10 +44,10 @@ require $root . '/partials/header.php';
             </div>
         </div>
     </div>
-</section> -->
+</section>
 
 <!-- ESTUDO -->
-<!-- <section id="estudo">
+<section id="estudo">
     <div class="container px-4 py-5">
         <div class="row">
             <div class="col-12">
@@ -75,7 +75,7 @@ require $root . '/partials/header.php';
             </div>
         </div>
     </div>
-</section> -->
+</section>
 
 <!-- THUMB SECAO -->
 <section id="thumb-secao">
@@ -85,6 +85,99 @@ require $root . '/partials/header.php';
                 <img loading="lazy" class="img-fluid" src="" alt="LAPTOP">
                 <img loading="lazy" class="img-fluid" src="" alt="Dados tecnicos">
             </figure>
+        </div>
+    </div>
+</section>
+
+<!-- MOTIVOS ATLANTIC -->
+<section id="motivos">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col">
+                <h2>Por que escolher a Atlantic Hub?</h2>
+
+                <p>Temos experiência, uma equipe especializada e metodologias para prospectar oportunidades e identificar riscos. A Atlantic Hub acompanha as empresas em todas as etapas de internacionalização.
+                </p>
+
+                <p>Com um relatório completo sobre o seu segmento de atuação em Portugal,que envolve dados técnicos do mercado, comportamento da concorrência e informações relevantes da cultura empresarial do país, a decisão de internacionalizar os seus negócios será muito mais segura.</p>
+            </div>
+
+            <div class="col-12">
+                <h3>A QUEM SE DESTINA:</h3>
+
+                <ul class="p-0">
+                    <li>→ Empresários que buscam a internacionalização;</li>
+                    <li>→ Início do processo PT2020;</li>
+                    <li>→ Elaboração de Business Plan (VISTOD2).</li>
+                </ul>
+
+                <a href="" target="_blank">ADQUIRA AGORA</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- CASES -->
+<section id="cases">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col">
+                <span>
+                    São + de
+                    <strong>
+                        100
+                    </strong>
+                    Empresas que já fizeram o Atlantic MarketFit
+                </span>
+
+                <h2>cases</h2>
+            </div>
+
+            <figure class="col-12 mb-0">
+                <img loading="lazy" class="img-fluid" src="" alt="Video Cases">
+                <img loading="lazy" class="img-fluid" src="" alt="Video Cases">
+            </figure>
+        </div>
+    </div>
+</section>
+
+<!-- CRONOGRAMA -->
+<section id="cronograma">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col">
+                <h2>CRONOGRAMA</h2>
+                <p>Etapa 1: Recebimento dos dados e informações da empresa.</p>
+                <p>Etapa 2: Levantamento dos dados em Portugal.</p>
+                <p>Etapa 3: Elaboração do relatório.</p>
+                <p>Etapa 4: Envio e apresentação do relatório.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- INVESTIMENTO -->
+<section id="investimento">
+    <div class="container px-4 py-5">
+        <div class="row">
+            <div class="col">
+                <h2>Quanto você
+                    vai investir inicialmente?</h2>
+            </div>
+
+            <div class="col-12">
+                <img loading="lazy" class="img-fluid" src="" alt="Icone investimento">
+                <p>Para você, empresário brasileiro, quer iniciar a jornada de internacionalização, o relatório Atlantic MarketFit requer um investimento inicial de € 1.750,00 </p>
+            </div>
+
+            <div class="col-12">
+                <img loading="lazy" class="img-fluid" src="" alt="Icone Prazo">
+                <p>O prazo de resposta da Atlantic Hub é de 15 dias úteis, após o recebimento de todas as informações obrigatórias.</p>
+            </div>
+
+            <div class="col-12">
+                <a href="" target="_blank">CONTRATE AGORA</a>
+            </div>
         </div>
     </div>
 </section>
