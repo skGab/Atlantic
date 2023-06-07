4e5fa329c8b0e09b218216afa9e829aeb4c72b87
zdeep10 <gabriel.wti02@gmail.com>
2023-06-07 17:47:01 -0300

    Component Footer

--- partials/footer.php
+++ partials/footer.php
@@ -1,60 +1,80 @@
 </main>
 
 <!-- FOOTER -->
-<footer id="footer">
-    <div class="container px-4 py-5">
+<footer id="footer" class="pt-5">
+    <div class="container px-4 pt-4 pb-5">
         <!-- ENDERECO E SOCIAL -->
-        <div class="row">
+        <div class="row space">
             <div class="col">
-                <figure class="logo">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Atlantic">
+                <figure class="logo space">
+                    <img loading="lazy" class="img-fluid" width="170px" src="../assets/images/logo-atlantic-footer.png" alt="Logo Atlantic">
                 </figure>
 
-                <div class="endereco">
+                <div class="endereco space lh-1 small text-white-footer">
                     <p>Contato: +351 910 297 331</p>
                     <p>Edifício Parque Oceano, Av. Marginal 2 4º</p>
                     <p>A, 2780-337 Lisboa, Portugal</p>
                 </div>
 
-                <div class="social">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo facebook">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Instagram">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Youtube">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Twitter">
-                </div>
+                <figure class="social d-flex gap-3 mb-0">
+                    <a href="" target="_blank">
+                        <img loading="lazy" class="img-fluid" src="../assets/icons/facebook-footer.svg" alt="Logo facebook">
+                    </a>
+                    <a href="" target="_blank">
+                        <img loading="lazy" class="img-fluid" src="../assets/icons/instagram-footer.svg" alt="Logo Instagram">
+                    </a>
+                    <a href="" target="_blank">
+                        <img loading="lazy" class="img-fluid" src="../assets/icons/youtube-footer.svg" alt="Logo Youtube">
+                    </a>
+                    <a href="" target="_blank">
+                        <img loading="lazy" class="img-fluid" src="../assets/icons/twitter-footer.svg" alt="Logo Twitter">
+                    </a>
+                </figure>
             </div>
         </div>
         <!-- NAVEGACAO -->
-        <div class="row">
+        <div class="row space">
             <div class="col">
-                <nav>
-                    <a href="">Home</a>
-                    <a href="">Quem Somos</a>
-                    <a href="">Estudo de Mercado</a>
-                    <a href="">Blog</a>
-                    <a href="">Contato</a>
-                    <a href="">Vagas</a>
+                <nav class="d-flex flex-column lh-lg">
+                    <a href="" class="text-white-footer">Home</a>
+                    <a href="" class="text-white-footer">Quem Somos</a>
+                    <a href="" class="text-white-footer">Estudo de Mercado</a>
+                    <a href="" class="text-white-footer">Blog</a>
+                    <a href="" class="text-white-footer">Contato</a>
+                    <a href="" class="text-white-footer">Vagas</a>
                 </nav>
             </div>
         </div>
         <!-- DIREITOS -->
         <div class="row">
             <div class="col">
-                <p><strong>Atlantic Hub |</strong> © Copyright 2022 - Todos os direitos reservados</p>
-                <p><strong>Desenvolvido por</strong> <img loading="lazy" class="img-fluid" src="" alt="Logo Black Beans"></p>
+                <p class="text-white-footer"><strong class="text-white letter">Atlantic Hub |</strong> © Copyright 2022 - Todos Os Direitos Reservados</p>
+                <div class="d-flex align-items-center">
+                    <strong class="text-white me-2 letter">Desenvolvido por</strong>
+                    <a href="https://blackbeans.com.br/" target="_blank"><img class="mb-1" loading="lazy" src="../assets/icons/logo-blackbeans.png" alt="Logo Black Beans"></a>
+                </div>
             </div>
         </div>
+    </div>
+
+    <div class="container">
         <!-- PATROCINEO -->
-        <div class="row">
-            <figure class="col mb-0">
-                <p>Certificação:
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo IAPMEI">
-                </p>
-                <p>Membro:
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Câmara">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Câmara Municipal">
-                    <img loading="lazy" class="img-fluid" src="" alt="Logo Rio Grande">
-                </p>
+        <div class="row patrocineo px-3 py-5">
+            <figure class="col mb-0 text-primary">
+                <div class="d-flex align-items-center mb-4">
+                    <p class="me-3 mb-0">
+                        Certificação:
+                    </p>
+                    <img loading="lazy" class="img-fluid" src="../assets/images/logo_iapmei.png" alt="Logo IAPMEI">
+                </div>
+                <div class="d-flex align-items-center gap-3">
+                    <p class="mb-0">
+                        Membro:
+                    </p>
+                    <img loading="lazy" class="img-fluid" src="../assets/images/logo_camara.png" alt="Logo Câmara">
+                    <img loading="lazy" class="img-fluid" src="../assets/images/logo_footer1.png" alt="Logo Câmara Municipal">
+                    <img loading="lazy" class="img-fluid" src="../assets/images/logo_footer2.png" alt="Logo Rio Grande">
+                </div>
             </figure>
         </div>
     </div>
