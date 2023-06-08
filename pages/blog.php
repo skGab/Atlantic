ecbb097aee4103c32d45d3b046c006debec82eb7
Amanda Mello <amandaricmello@gmail.com>
2023-06-08 16:43:52 -0300

    blog page structure

---
+++ pages/blog.php
@@ -0,0 +1,263 @@
+<?php
+require("../partials/header.php");
+?>
+
+<!-- ARTIGOS EM DESTAQUE -->
+<section id="destaque-artigos" class="container py-5 mt-5">
+    <div class="row w-100 mx-0">
+        <div class="col-12">
+            <h1 class="text-primary fw-normal text-uppercase display-4 mb-3">Acompanhe as noticias</h1>
+        </div>
+        <div class="col-12 d-flex align-items-center w-100 mb-4">
+            <a href="#" class="text-secondary bg-light rounded-pill px-4 py-1 me-2">Categorias</a>
+            <a href="#" class="text-secondary bg-light rounded-pill px-4 py-1 me-2">Tags</a>
+        </div>
+
+        <div class="col-12 mb-4">
+            <div class="card bg-light w-100 rounded-0 border-0">
+                <div class="position-relative">
+                    <img src="../assets/images/img-post1.jpg" class="card-img-top rounded-0" alt="Capa do Post">
+                    <div class="info-post">
+                        <a href="#" class="small text-secondary bg-secondary text-light fw-light rounded-pill px-3 py-1 me-2">Nome da tag</a>
+                        <time datetime="2021-12-13" class="small text-secondary bg-light rounded-pill px-3 py-1 me-2">13 de dez. 2021</time>
+                    </div>
+                </div>
+                <div class="box1"></div>
+                <div class="card-body px-4 pb-4">
+                    <h2 class="h5 card-title text-secondary">Portugal, um dos países queridinhos para congressos e convenções</h2>
+                    <p class="card-text small text-primary">
+                        De acordo com o relatório da Associação Internacional de Congressos...
+                    </p>
+                    <a href="#" class="text-secondary d-flex w-100 align-items-center">
+                        Ler mais
+                        <span class="material-symbols-outlined ms-3" style="font-size: 30px;">
+                            arrow_right_alt
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 mb-4">
+            <div class="card bg-light w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-6">
+                        <img src="../assets/images/img-post2.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-6">
+                        <div class="card-body">
+                            <h2 class="h5 card-title text-secondary mb-0">Atlantic Connection</h2>
+                            <time datetime="2021-12-13" class="card-text small text-primary">13 de dez. 2021</time>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center mt-2">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-1" style="font-size: 30px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <div class="card bg-light w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-6">
+                        <img src="../assets/images/img-post2.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-6">
+                        <div class="card-body">
+                            <h2 class="h5 card-title text-secondary mb-0">Atlantic Connection</h2>
+                            <time datetime="2021-12-13" class="card-text small text-primary">13 de dez. 2021</time>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center mt-2">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-1" style="font-size: 30px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- NOTÍCIAS -->
+<section id="noticias" class="container py-5">
+    <div class="row w-100 mx-0">
+        <div class="col-12 pe-5">
+            <h1 class="text-primary fw-normal text-uppercase display-4 mb-3 pe-5">Acompanhe as noticias</h1>
+        </div>
+        <div class="col-12 d-flex align-items-center w-100 mb-4 pb-2">
+            <a href="#" class="me-2">
+                <img src="../assets/icons/facebook.svg" alt="ícone do Facebook">
+            </a>
+            <a href="#" class="me-2">
+                <img src="../assets/icons/instagram.svg" alt="Ícone do Instagram">
+            </a>
+            <a href="#" class="me-2">
+                <img src="../assets/icons/youtube.svg" alt="Ícone do Youtube">
+            </a>
+            <a href="#" class="me-2">
+                <img src="../assets/icons/twitter.svg" alt="Ícone do Twitter">
+            </a>
+        </div>
+
+        <div class="col-12 mb-5">
+            <div class="card bg-transparent w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-5">
+                        <img src="../assets/images/img-post3.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-7">
+                        <div class="card-body py-0">
+                            <h2 class="h5 card-title text-secondary mb-2 pb-1">
+                                Portugal, um dos países queridinhos para congressos e convenções
+                            </h2>
+                            <p class="card-text small text-primary mb-0">
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum...
+                            </p>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-4" style="font-size: 40px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+        <div class="col-12 mb-5">
+            <div class="card bg-transparent w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-5">
+                        <img src="../assets/images/img-post3.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-7">
+                        <div class="card-body py-0">
+                            <h2 class="h5 card-title text-secondary mb-2 pb-1">
+                                Portugal, um dos países queridinhos para congressos e convenções
+                            </h2>
+                            <p class="card-text small text-primary mb-0">
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum...
+                            </p>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-4" style="font-size: 40px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+        <div class="col-12 mb-5">
+            <div class="card bg-transparent w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-5">
+                        <img src="../assets/images/img-post3.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-7">
+                        <div class="card-body py-0">
+                            <h2 class="h5 card-title text-secondary mb-2 pb-1">
+                                Portugal, um dos países queridinhos para congressos e convenções
+                            </h2>
+                            <p class="card-text small text-primary mb-0">
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum...
+                            </p>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-4" style="font-size: 40px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+        <div class="col-12 mb-5">
+            <div class="card bg-transparent w-100 rounded-0 border-0 position-relative">
+                <div class="row g-0">
+                    <div class="col-5">
+                        <img src="../assets/images/img-post3.jpg" class="d-block w-100 h-100 rounded-0" alt="Capa do Post">
+                    </div>
+                    <div class="col-7">
+                        <div class="card-body py-0">
+                            <h2 class="h5 card-title text-secondary mb-2 pb-1">
+                                Portugal, um dos países queridinhos para congressos e convenções
+                            </h2>
+                            <p class="card-text small text-primary mb-0">
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum...
+                            </p>
+                            <a href="#" class="text-secondary d-flex w-100 align-items-center">
+                                Ler mais
+                                <span class="material-symbols-outlined ms-4" style="font-size: 40px;">
+                                    arrow_right_alt
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box2"></div>
+            </div>
+        </div>
+
+        <nav aria-label="Navegação dos posts">
+            <ul class="pagination justify-content-center align-items-center mt-2 mb-0">
+                <li class="page-item">
+                    <a class="px-3" href="#">
+                        <img src="../assets/icons/double-arrow-left.svg" alt="Ícone de link para a primeira página de posts">
+                    </a>
+                </li>
+                <li class="page-item">
+                    <a class="px-3" href="#">
+                        <img src="../assets/icons/arrow-left.svg" alt="Ícone de link para a página de posts anterior">
+                    </a>
+                </li>
+                <li class="page-item active">
+                    <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">4</a>
+                </li>
+                <li class="page-item">
+                    <a class="page-link" href="#">5</a>
+                </li>
+                <li class="page-item">
+                    <a class="px-3" href="#">
+                        <img src="../assets/icons/arrow-right.svg" alt="Ícone de link para a próxima página de posts">
+                    </a>
+                </li>
+                <li class="page-item">
+                    <a class="px-3" href="#">
+                        <img src="../assets/icons/double-arrow-right.svg" alt="Ícone de link para a última página de posts">
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</section>
+
+<?php
+require("../partials/footer.php");
+?>
