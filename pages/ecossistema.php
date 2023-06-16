6b05d27003ae9eb86849ab5e62c06c46ad27f00f
Amanda Mello <amandaricmello@gmail.com>
2023-06-16 16:19:01 -0300

    ecosystem page mobile structure

--- pages/ecossistema.php
+++ pages/ecossistema.php
@@ -1,100 +1,124 @@
 <?php
-$root = $_SERVER['DOCUMENT_ROOT'];
-require $root . '/partials/header.php';
+require('../partials/header.php');
 ?>
+
+<!-- BANNER INTRO -->
+<section id="banner-ecossistema" class="banner justify-content-center pt-5">
+    <!-- <div class="mask-banner"></div> -->
+    <div class="container">
+        <div class="row w-100">
+            <div class="title-banner col-lg-1 col-4 ms-lg-5 ms-3">
+                <h1 class="text-white text-uppercase">Ecossistema</h1>
+                <span>Ecossistema</span>
+            </div>
+        </div>
+    </div>
+</section>
 
 <!-- ECOSSISTEMA -->
 <section id="ecossistema">
     <div class="container px-4 py-5">
         <div class="row">
-            <div class="col">
-                <h2>Ter um ecossistema sólido implica uma rede de vínculos e apoios que ajudam a superar os desafios.</h2>
+            <div class="col text-primary">
+                <h2 class="text-primary">Ter um ecossistema sólido implica uma rede de vínculos e apoios que ajudam a superar os desafios.</h2>
 
                 <p>Ao fazer parte de um ecossistema empresarial robusto, as empresas podem ter acesso a recursos, conhecimentos e expertise necessários para enfrentar a complexidade dos processos com mais eficiência e eficácia.</p>
 
                 <p>A Atlantic Hub integra uma rede de parcerias e que promove oportunidades para o mercado global e facilita o sucesso da internacionalização dos seus negócios.</p>
             </div>
 
-            <figure class="col-12 mb-0">
-                <img loading="lazy" class="img-fluid" src="" alt="Thumb ecossistema">
+            <figure class="col-12 mb-0 mt-3">
+                <img loading="lazy" class="img-fluid" src="../assets/images/thumb-ecossitema.png" alt="Thumb ecossistema">
             </figure>
         </div>
     </div>
 </section>
 
 <!-- PARCEIROS -->
-<section id="parceiros">
+<section id="parceiros" class="d-block bg-dark">
     <div class="container px-4 py-5">
         <div class="row">
-            <div class="col">
-                <h2>PARCEIROS</h2>
+            <div class="col mb-5">
+                <h1 class="display-4 text-primary">PARCEIROS</h1>
             </div>
 
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Atlantic Station</h4>
-                        <p class="card-text">Um Hub de Inovação que gera conexões entre os atores dos ecossistemas brasileiro e europeu.
-                            O endereço fiscal da sua empresa em um ambiente de troca de experiências e geração de negócios.
-                        </p>
+            <div class="col-12 mb-4">
+                <div class="container-fluid px-0">
+                    <div class="row w-100 mx-0">
+                        <div class="col-5">
+                            <img loading="lazy" class="d-block w-100" src="../assets/images/parceiros/atlanticStation_logo.png" alt="Card Logo">
+                        </div>
+                        <div class="col-7 px-0 pt-2">
+                            <h4 class="text-secondary">Atlantic Station</h4>
+                            <p class="text-primary small">Um Hub de Inovação que gera conexões entre os atores dos ecossistemas brasileiro e europeu.
+                                O endereço fiscal da sua empresa em um ambiente de troca de experiências e geração de negócios.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mb-4">
+                <div class="container-fluid px-0">
+                    <div class="row w-100 mx-0">
+                        <div class="col-5">
+                            <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/conexao_logo_white.png" alt="Card Logo">
+                        </div>
+                        <div class="col-7 px-0 pt-1">
+                            <h4 class="text-secondary">Conexão Europa Imóveis</h4>
+                            <p class="text-primary small">
+                                Experiência de mercado e os contatos necessários para a compra, venda ou locação de imóveis, para morar ou investir em Portugal.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mb-4">
+                <div class="container-fluid px-0">
+                    <div class="row w-100 mx-0">
+                        <div class="col-5">
+                            <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/fcj_logo.png" alt="Card Logo">
+                        </div>
+                        <div class="col-7 px-0 pt-1">
+                            <h4 class="text-secondary">FCJ Portugal</h4>
+                            <p class="text-primary small">
+                                A FCJ Venture Builder é a maior Venture Builder da América Latina, são responsáveis por conectarem startups, investidores, corporações e universidades para desenvolver negócios que vão impactar a vida das pessoas.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 mb-4">
+                <div class="container-fluid px-0">
+                    <div class="row w-100 mx-0">
+                        <div class="col-5">
+                            <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/webSummit_logo.png" alt="Card Logo">
+                        </div>
+                        <div class="col-7 px-0 pt-1">
+                            <h4 class="text-secondary">Missão Web Summit</h4>
+                            <p class="text-primary small">
+                                A Missão Web Summit é uma missão de negócios, tecnologia e inovação que conecta startups e empresários brasileiros com o ecossistema empreendedor de Portugal que figura entre os melhores da Europa.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Conexão Europa Imóveis</h4>
-                        <p class="card-text">Experiência de mercado e os contatos necessários para a compra, venda ou locação de imóveis, para morar ou investir em Portugal.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">FCJ Portugal</h4>
-                        <p class="card-text">A FCJ Venture Builder é a maior Venture Builder da América Latina, são responsáveis por conectarem startups, investidores, corporações e universidades para desenvolver negócios que vão impactar a vida das pessoas.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Missão Web Summit</h4>
-                        <p class="card-text">A Missão Web Summit é uma missão de negócios, tecnologia e inovação que conecta startups e empresários brasileiros com o ecossistema empreendedor de Portugal que figura entre os melhores da Europa.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Portugal Ventures</h4>
-                        <p class="card-text">Uma empresa com a expertise de desenvolver estratégias diferenciadas de crescimento, competitividade e internacionalização, buscando impulsionar o sucesso global das empresas.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Vistos e nacionalidade Portuguesa</h4>
-                        <p class="card-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="card d-flex flex-row">
-                    <img loading="lazy" class="img-fluid w-100" src="" alt="Card Logo">
-                    <div class="card-body p-0">
-                        <h4 class="card-title">Câmbio</h4>
-                        <p class="card-text">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                            officia deserunt mollit anim id est laborum.</p>
+                <div class="container-fluid px-0">
+                    <div class="row w-100 mx-0">
+                        <div class="col-5">
+                            <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/portugalVentures_logo.png" alt="Card Logo">
+                        </div>
+                        <div class="col-7 px-0 pt-0">
+                            <h4 class="text-secondary">Portugal Ventures</h4>
+                            <p class="text-primary small">Uma empresa com a expertise de desenvolver estratégias diferenciadas de crescimento, competitividade e internacionalização, buscando impulsionar o sucesso global das empresas.</p>
+                            <h4 class="text-secondary">Vistos e nacionalidade Portuguesa</h4>
+                            <p class="text-primary small">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.
+                            </p>
+                            <h4 class="text-secondary">Câmbio</h4>
+                            <p class="text-primary small">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+                                officia deserunt mollit anim id est laborum.</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -103,26 +127,34 @@ require $root . '/partials/header.php';
 </section>
 
 <!-- NOVIDADES -->
-<section id="novidades">
+<section id="contato" class="d-block bg-light pt-4 pb-5">
     <div class="container px-4 py-5">
-        <div class="row">
-            <div class="col">
-                <h2>Receba nossas novidades</h2>
+        <div class="row px-3">
+            <div class="col-12">
+                <h2 class="text-secondary display-4">RECEBA NOSSAS NOVIDADES</h2>
+                <figure class="position-relative mb-0">
+                    <img loading="lazy" class="img-cover" src="../assets/images/thumb-contato.jpg" alt="Thumb contato">
+                    <div class="mask-contato"></div>
+                    <div class="box"></div>
+                </figure>
+            </div>
 
+            <div id="form-candidatura" class="col-12 mt-5">
                 <form>
-                    <figure>
-                        <img loading="lazy" class="img-fluid" src="" alt="Thumb Novidades">
-                    </figure>
-
                     <div class="mb-3">
-                        <input placeholder="Nome" type="text" class="form-control" id="name" aria-describedby="nameHelp">
+                        <input placeholder="Nome*" type="text" class="form-control" id="name" aria-describedby="nameHelp">
                     </div>
 
                     <div class="mb-3">
-                        <input placeholder="Email" type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                        <input placeholder="Email*" type="email" class="form-control" id="email" aria-describedby="emailHelp">
                     </div>
 
-                    <button type="submit" class="bg-transparent border-0 btn">ENVIAR <img src="" alt="Seta Direita"></button>
+                    <button type="submit" class="btn text-secondary d-flex justify-content-lg-end align-items-center bg-transparent ms-auto px-0 pt-0">
+                        Enviar
+                        <span class="material-symbols-outlined ms-2" style="font-size: 35px;">
+                            arrow_right_alt
+                        </span>
+                    </button>
                 </form>
             </div>
         </div>
@@ -130,6 +162,5 @@ require $root . '/partials/header.php';
 </section>
 
 <?php
-$root = $_SERVER['DOCUMENT_ROOT'];
-require $root . '/partials/footer.php';
+require('../partials/footer.php');
 ?>
