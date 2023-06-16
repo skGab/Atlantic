1a1519aa6242e5275e1c3aa475eac08151eee5c3
Amanda Mello <amandaricmello@gmail.com>
2023-06-16 16:35:48 -0300

    ecosystem page desktop structure

--- pages/ecossistema.php
+++ pages/ecossistema.php
@@ -17,9 +17,9 @@ require('../partials/header.php');
 
 <!-- ECOSSISTEMA -->
 <section id="ecossistema">
-    <div class="container px-4 py-5">
-        <div class="row">
-            <div class="col text-primary">
+    <div class="container px-lg-0 px-4 py-5">
+        <div class="row align-items-lg-center justify-content-lg-between">
+            <div class="col-lg-6 col-12 text-primary">
                 <h2 class="text-primary">Ter um ecossistema sólido implica uma rede de vínculos e apoios que ajudam a superar os desafios.</h2>
 
                 <p>Ao fazer parte de um ecossistema empresarial robusto, as empresas podem ter acesso a recursos, conhecimentos e expertise necessários para enfrentar a complexidade dos processos com mais eficiência e eficácia.</p>
@@ -27,28 +27,28 @@ require('../partials/header.php');
                 <p>A Atlantic Hub integra uma rede de parcerias e que promove oportunidades para o mercado global e facilita o sucesso da internacionalização dos seus negócios.</p>
             </div>
 
-            <figure class="col-12 mb-0 mt-3">
-                <img loading="lazy" class="img-fluid" src="../assets/images/thumb-ecossitema.png" alt="Thumb ecossistema">
+            <figure class="col-lg-5 col-12 mb-0 mt-3">
+                <img loading="lazy" class="d-block w-100" src="../assets/images/thumb-ecossistema.png" alt="Thumb ecossistema">
             </figure>
         </div>
     </div>
 </section>
 
 <!-- PARCEIROS -->
-<section id="parceiros" class="d-block bg-dark">
+<section id="parceiros" class="d-block bg-dark py-lg-5">
     <div class="container px-4 py-5">
         <div class="row">
-            <div class="col mb-5">
+            <div class="col-12 mb-5 ps-lg-4">
                 <h1 class="display-4 text-primary">PARCEIROS</h1>
             </div>
 
-            <div class="col-12 mb-4">
+            <div class="col-lg-9 col-12 mb-4">
                 <div class="container-fluid px-0">
                     <div class="row w-100 mx-0">
-                        <div class="col-5">
+                        <div class="col-lg-2 col-5">
                             <img loading="lazy" class="d-block w-100" src="../assets/images/parceiros/atlanticStation_logo.png" alt="Card Logo">
                         </div>
-                        <div class="col-7 px-0 pt-2">
+                        <div class="col-lg-9 col-7 pe-lg-3 px-0 pt-2">
                             <h4 class="text-secondary">Atlantic Station</h4>
                             <p class="text-primary small">Um Hub de Inovação que gera conexões entre os atores dos ecossistemas brasileiro e europeu.
                                 O endereço fiscal da sua empresa em um ambiente de troca de experiências e geração de negócios.
@@ -57,13 +57,13 @@ require('../partials/header.php');
                     </div>
                 </div>
             </div>
-            <div class="col-12 mb-4">
+            <div class="col-lg-9 col-12 mb-4">
                 <div class="container-fluid px-0">
                     <div class="row w-100 mx-0">
-                        <div class="col-5">
+                        <div class="col-lg-2 col-5">
                             <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/conexao_logo_white.png" alt="Card Logo">
                         </div>
-                        <div class="col-7 px-0 pt-1">
+                        <div class="col-lg-9 col-7 pe-lg-3 px-0 pt-1">
                             <h4 class="text-secondary">Conexão Europa Imóveis</h4>
                             <p class="text-primary small">
                                 Experiência de mercado e os contatos necessários para a compra, venda ou locação de imóveis, para morar ou investir em Portugal.
@@ -72,13 +72,13 @@ require('../partials/header.php');
                     </div>
                 </div>
             </div>
-            <div class="col-12 mb-4">
+            <div class="col-lg-9 col-12 mb-4">
                 <div class="container-fluid px-0">
                     <div class="row w-100 mx-0">
-                        <div class="col-5">
+                        <div class="col-lg-2 col-5">
                             <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/fcj_logo.png" alt="Card Logo">
                         </div>
-                        <div class="col-7 px-0 pt-1">
+                        <div class="col-lg-9 col-7 pe-lg-3 px-0 pt-1">
                             <h4 class="text-secondary">FCJ Portugal</h4>
                             <p class="text-primary small">
                                 A FCJ Venture Builder é a maior Venture Builder da América Latina, são responsáveis por conectarem startups, investidores, corporações e universidades para desenvolver negócios que vão impactar a vida das pessoas.
@@ -87,13 +87,13 @@ require('../partials/header.php');
                     </div>
                 </div>
             </div>
-            <div class="col-12 mb-4">
+            <div class="col-lg-9 col-12 mb-4">
                 <div class="container-fluid px-0">
                     <div class="row w-100 mx-0">
-                        <div class="col-5">
+                        <div class="col-lg-2 col-5">
                             <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/webSummit_logo.png" alt="Card Logo">
                         </div>
-                        <div class="col-7 px-0 pt-1">
+                        <div class="col-lg-9 col-7 pe-lg-3 px-0 pt-1">
                             <h4 class="text-secondary">Missão Web Summit</h4>
                             <p class="text-primary small">
                                 A Missão Web Summit é uma missão de negócios, tecnologia e inovação que conecta startups e empresários brasileiros com o ecossistema empreendedor de Portugal que figura entre os melhores da Europa.
@@ -102,13 +102,13 @@ require('../partials/header.php');
                     </div>
                 </div>
             </div>
-            <div class="col-12">
+            <div class="col-lg-9 col-12">
                 <div class="container-fluid px-0">
                     <div class="row w-100 mx-0">
-                        <div class="col-5">
+                        <div class="col-lg-2 col-5">
                             <img loading="lazy" class="d-block w-75 mx-auto" src="../assets/images/parceiros/portugalVentures_logo.png" alt="Card Logo">
                         </div>
-                        <div class="col-7 px-0 pt-0">
+                        <div class="col-lg-9 col-7 pe-lg-3 px-0 pt-0">
                             <h4 class="text-secondary">Portugal Ventures</h4>
                             <p class="text-primary small">Uma empresa com a expertise de desenvolver estratégias diferenciadas de crescimento, competitividade e internacionalização, buscando impulsionar o sucesso global das empresas.</p>
                             <h4 class="text-secondary">Vistos e nacionalidade Portuguesa</h4>
@@ -127,7 +127,7 @@ require('../partials/header.php');
 </section>
 
 <!-- NOVIDADES -->
-<section id="contato" class="d-block bg-light pt-4 pb-5">
+<section id="contato" class="d-lg-none d-block bg-light pt-4 pb-5">
     <div class="container px-4 py-5">
         <div class="row px-3">
             <div class="col-12">
@@ -157,6 +157,49 @@ require('../partials/header.php');
                     </button>
                 </form>
             </div>
+        </div>
+    </div>
+</section>
+
+<section id="contato" class="d-lg-block d-none bg-light">
+    <div class="container-fluid p-0">
+        <div class="row align-items-center w-100 mx-0 px-0">
+            <div class="col-4 px-0">
+                <figure class="position-relative mb-0">
+                    <img loading="lazy" class="d-block w-100" src="../assets/images/thumb-contato.jpg" alt="Thumb contato">
+                    <div class="mask-contato"></div>
+                    <div class="box" style="width: 82px; height: 82px; right: -2.5rem; bottom: 4rem;"></div>
+                </figure>
+            </div>
+            <div class="col-6 ms-3" style="z-index: 10;">
+                <h2 class="text-secondary d-flex display-4 mb-4" style="margin-left: -5rem;">RECEBA NOSSAS NOVIDADES</h2>
+                <div id="form-candidatura" class="col-8">
+                    <form>
+                        <div class="row align-items-end">
+                            <div class="col-10">
+                                <div class="mb-3">
+                                    <input placeholder="Nome*" type="text" class="form-control" id="name" aria-describedby="nameHelp">
+                                </div>
+            
+                                <div class="mb-3">
+                                    <input placeholder="Email*" type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                                </div>
+                            </div>
+
+                            <div class="col-2 pb-3">
+                                <button type="submit" class="btn text-secondary d-flex justify-content-lg-end align-items-center bg-transparent ms-auto px-0 pt-0">
+                                    Enviar
+                                    <span class="material-symbols-outlined ms-2" style="font-size: 35px;">
+                                        arrow_right_alt
+                                    </span>
+                                </button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
         </div>
     </div>
 </section>
