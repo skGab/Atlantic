6233acca685484b4b13f22955c63ba44a5d4e744
Amanda Mello <amandaricmello@gmail.com>
2023-06-19 12:26:59 -0300

    layout adjustments

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,8 @@
 <?php
 $current_page =  $_SERVER['REQUEST_URI'];
 $root = $_SERVER['DOCUMENT_ROOT'];
+
+$home = get_stylesheet_directory_uri();
 ?>
 
 <!DOCTYPE html>
@@ -11,14 +13,14 @@ $root = $_SERVER['DOCUMENT_ROOT'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <meta name="description" content=""> -->
-    <link rel="shortcut icon" href="../assets/icons/fav-icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo $home ?>/assets/icons/fav-icon.png" type="image/x-icon">
     <title>Atlantic Hub</title>
 
     <!-- Bootstrap CSS -->
-    <link href="../assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://webapp355388.ip-192-155-89-190.cloudezapp.io/wp-content/themes/atlantichub/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Bootstrap JS -->
-    <script src="../assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script defer src="https://webapp355388.ip-192-155-89-190.cloudezapp.io/wp-content/themes/atlantichub/assets/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- SPLINE -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" integrity="sha256-5uKiXEwbaQh9cgd2/5Vp6WmMnsUr3VZZw0a8rKnOKNU=" crossorigin="anonymous">
@@ -26,9 +28,9 @@ $root = $_SERVER['DOCUMENT_ROOT'];
     <script defer src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js" integrity="sha256-FZsW7H2V5X9TGinSjjwYJ419Xka27I8XPDmWryGlWtw=" crossorigin="anonymous"></script>
 
     <!-- STYLE -->
-    <link rel="stylesheet" href="../assets/css/compressed/style.min.css">
-    <link rel="stylesheet" href="../assets/font.css">
-    <link rel="stylesheet" href="../assets/custom.css">
+    <link rel="stylesheet" href="https://webapp355388.ip-192-155-89-190.cloudezapp.io/wp-content/themes/atlantichub/assets/css/compressed/style.min.css">
+    <link rel="stylesheet" href="https://webapp355388.ip-192-155-89-190.cloudezapp.io/wp-content/themes/atlantichub/assets/font.css">
+    <link rel="stylesheet" href="https://webapp355388.ip-192-155-89-190.cloudezapp.io/wp-content/themes/atlantichub/assets/custom.css">
 
     <!-- Google Fonts Icons -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
@@ -60,11 +62,11 @@ $root = $_SERVER['DOCUMENT_ROOT'];
 
     <header id="header" class="position-fixed w-100">
         <div class="px-lg-5 px-3 mt-2">
-            <nav class="navbar navbar-light navbar-expand-lg bg-white py-0" aria-label="First navbar example">
+            <nav class="navbar navbar-light navbar-expand-lg bg-white py-0 mx-auto" aria-label="First navbar example">
                 <div class="container-fluid px-3 px-md-3 px-lg-4 px-xl-5">
                     <div class="row justify-content-between align-items-center w-100 mx-0">
                         <a class="navbar-brand col-lg-2 px-lg-0 col-5 me-0" href="/">
-                            <img class="d-block img-fluid" src="../assets/images/atlantic_logo.png" alt="Logo Atlantic Hub">
+                            <img class="d-block img-fluid" src="<?php echo $home ?>/assets/images/atlantic_logo.png" alt="Logo Atlantic Hub">
                         </a>
 
                         <button class="navbar-toggler border-0 col-3 text-end" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
@@ -73,7 +75,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                             </span>
                         </button>
 
-                        <div class="collapse navbar-collapse px-4 px-md-0 col-6" id="menu">
+                        <div class="collapse navbar-collapse px-4 px-lg-0 pe-md-5 col-6" id="menu">
                             <ul class="navbar-nav ms-lg-auto me-lg-0 ms-0 me-auto text-end mb-lg-0 mb-2 gap-xl-2 gap-xxl-3 gap-md-1 align-items-lg-center">
 
                                 <button class="navbar-toggler border-0 text-end pe-1 my-4 w-25 ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="menu" aria-expanded="false" aria-label="Toggle navigation">
@@ -85,13 +87,13 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                                     <a class="nav-link" href="/">Home</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/pages/quem-somos.php">Quem somos</a>
+                                    <a class="nav-link" href="/quem-somos/">Quem somos</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="/pages/atlantic-marketfit.php">Estudo de Mercado</a>
+                                    <a class="nav-link" href="/atlantic-marketfit/">Estudo de Mercado</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a class="nav-link pe-3 dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Momento</a>
+                                    <a class="nav-link pe-lg-3 pe-md-0 pe-3 dropdown-toggle" id="dropdown01" data-bs-toggle="dropdown" aria-expanded="false">Momento</a>
                                     <ul class="dropdown-menu" aria-labelledby="dropdown01">
                                         <li><a class="dropdown-item pe-lg-2 pe-3" href="#">Quero conhecer</a></li>
                                         <li><a class="dropdown-item pe-lg-2 pe-3" href="#">Já decidi e quero avançar</a></li>
@@ -99,26 +101,26 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                                     </ul>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link pe-3" href="/pages/ecossistema.php">Ecossistema</a>
+                                    <a class="nav-link pe-lg-3 pe-md-0 pe-3" href="/ecossistema/">Ecossistema</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link pe-3" href="/pages/blog.php">Blog</a>
+                                    <a class="nav-link pe-lg-3 pe-md-0 pe-3" href="/blog/">Blog</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link pe-3" href="/pages/contato.php">Contato</a>
+                                    <a class="nav-link pe-lg-3 pe-md-0 pe-3" href="/contato/">Contato</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link pe-3" href="/pages/vagas.php">Vagas</a>
+                                    <a class="nav-link pe-lg-3 pe-md-0 pe-3" href="/vagas/">Vagas</a>
                                 </li>
                                 <li class="nav-item d-lg-block d-none">
                                     <a class="nav-link" type="button" data-bs-toggle="modal" data-bs-target="#search-modal">
-                                        <img src="../assets/icons/icon-search-blue.svg" alt="">
+                                        <img src="<?php echo $home ?>/assets/icons/icon-search-blue.svg" alt="">
                                     </a>
                                 </li>
                             </ul>
                             <form class="search-form mb-5 pb-5 col-lg-2 d-lg-none d-flex">
                                 <input class="form-control search-form-input" type="text" placeholder="Pesquisar" aria-label="Pesquisar">
-                                <img src="../assets/icons/icon-search-white.svg" alt="">
+                                <img src="<?php echo $home ?>/assets/icons/icon-search-white.svg" alt="">
                             </form>
                         </div>
                     </div>
@@ -134,7 +136,7 @@ $root = $_SERVER['DOCUMENT_ROOT'];
                 <div class="modal-body">
                     <form class="search-form-modal mb-5 pb-5 d-flex">
                         <input class="form-control search-form-input-modal" type="text" placeholder="Escreva aqui..." aria-label="Pesquisar">
-                        <img src="../assets/icons/icon-search-blue.svg" alt="">
+                        <img src="<?php echo $home ?>/assets/icons/icon-search-blue.svg" alt="">
                     </form>
                 </div>
             </div>
